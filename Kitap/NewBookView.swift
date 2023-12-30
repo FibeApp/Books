ee93221ce4e28eda9cb52c6fc62088b0f95b5c4c
Vladimir Fibe <motiw@icloud.com>
2023-12-30 09:00:10 +0600

    подключил iCloud

--- Kitap/NewBookView.swift
+++ Kitap/NewBookView.swift
@@ -1,11 +1,25 @@
 import SwiftUI
 
 struct NewBookView: View {
+    @Environment(\.modelContext) private var context
+    @State private var title = ""
+    @State private var author = ""
     @Environment(\.dismiss) private var dismiss
     var body: some View {
         NavigationStack {
             Form {
-                Text("Hello, Book!")
+                TextField("Book Title", text: $title)
+                TextField("Author", text: $author)
+                Button("Create") {
+                    let newBook = Book(title: title, author: author)
+                    context.insert(newBook)
+                    dismiss()
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .buttonStyle(.borderedProminent)
+                .tint(.blue)
+                .padding(.vertical)
+                .disabled(title.isEmpty || author.isEmpty)
             }
             .navigationTitle("New Book")
             .navigationBarTitleDisplayMode(.inline)
